80a0da69f686a5153c967869604bd3a105e31e82
deepbelwal <deepak.belwal@nineleaps.com>
2019-10-18 12:21:39 +0530

    module3 completed

---
+++ webWorkerExample/src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import isPrimeNumber from 'prime-number';
+import primeNumberList from 'prime-number/list';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  runWorker() {
+    const worker = new Worker('./prime-calculations.worker', { 
+        type: 'module' 
+    });
+    worker.onmessage = ({ data }) => {
+        console.log('From Web Worker:', data);
+    };
+    worker.postMessage({});
+ }
+ runThread() {
+     const arePrimeList = primeNumberList.map((prime) => {
+         return isPrimeNumber(prime);
+     });
+     console.log('From Javascript Thread', arePrimeList);
+ }
+}
